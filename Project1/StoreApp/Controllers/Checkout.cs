9504dbbf9166c5520b2d02e4fcef8675ab766224
Jayson Lennon <jayson@jaysonlennon.dev>
2020-05-13 18:35:30 -0700

    Add item listing on checkout page

--- Project1/StoreApp/Controllers/Checkout.cs
+++ Project1/StoreApp/Controllers/Checkout.cs
@@ -12,34 +12,52 @@ namespace StoreApp.Controllers
 {
     public class Checkout : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<Checkout> _logger;
+        private IServiceProvider _services;
 
-        public Checkout(ILogger<HomeController> logger)
+        public Checkout(
+            ILogger<Checkout> logger,
+            IServiceProvider services
+            )
         {
-            _logger = logger;
+            this._logger = logger;
+            this._services = services;
         }
 
         [Route("Checkout")]
         [Authorize(Roles = Auth.Role.Customer)]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            // TODO: Replace dummy data with order information.
-            // TODO: Format items
-            var cartItem = new Models.CartItem();
-            cartItem.Id = Guid.NewGuid();
-            cartItem.Name = "item name here (test item)";
-            cartItem.UnitPrice = 12.34;
-            cartItem.Quantity = 4;
+            var locationRepo = (Repository.ILocation)this._services.GetService(typeof(Repository.ILocation));
+            var customerRepo = (Repository.ICustomer)this._services.GetService(typeof(Repository.ICustomer));
+            var orderRepo = (Repository.IOrder)this._services.GetService(typeof(Repository.IOrder));
+            var productRepo = (Repository.IProduct)this._services.GetService(typeof(Repository.IProduct));
 
-            var cartItem2 = new Models.CartItem();
-            cartItem2.Id = Guid.NewGuid();
-            cartItem2.Name = "another item";
-            cartItem2.UnitPrice = 3.0;
-            cartItem2.Quantity = 3;
+            var customerId = Guid.Parse(HttpContext.User.FindFirst(claim => claim.Type == Auth.Claim.UserId).Value);
+            _logger.LogDebug($"customer id={customerId}");
+
+            var customer = await customerRepo.GetCustomerById(customerId);
+            _logger.LogDebug($"customer obj={customer}");
+            var location = await customerRepo.GetDefaultLocation(customer);
+            _logger.LogDebug($"location obj={location}");
+
+            var currentOrder = await customerRepo.GetOpenOrder(customer, location);
+            _logger.LogDebug($"current order obj={currentOrder}");
+            var orderLines = orderRepo.GetOrderLines(currentOrder);
 
             var model = new Models.Checkout();
-            model.Items.Add(cartItem);
-            model.Items.Add(cartItem2);
+
+            foreach(var item in orderLines)
+            {
+                _logger.LogDebug($"iterate through item {item.Product.ProductId}");
+                var checkoutItem = new Models.CartItem();
+                checkoutItem.Id = item.Product.ProductId;
+                checkoutItem.Name = item.Product.Name;
+                checkoutItem.UnitPrice = item.Product.Price;
+                checkoutItem.Quantity = item.Quantity;
+                checkoutItem.ImageName = item.Product.ImageName;
+                model.Items.Add(checkoutItem);
+            }
 
             return View("Checkout", model);
         }
