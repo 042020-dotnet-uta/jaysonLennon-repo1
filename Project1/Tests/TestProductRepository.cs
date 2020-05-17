7f6c1339a4488cd06ff0bddb57546b06e737812e
Jayson Lennon <jayson@jaysonlennon.dev>
2020-05-16 22:15:02 -0700

    Add product test

--- Project1/Tests/TestProductRepository.cs
+++ Project1/Tests/TestProductRepository.cs
@@ -4,6 +4,7 @@ using Xunit;
 using Moq;
 
 using StoreApp.Data;
+using StoreApp.Entity;
 using StoreApp.Repository;
 
 namespace TestStoreApp
@@ -11,27 +12,22 @@ namespace TestStoreApp
     public class TestProductRepository
     {
         [Fact]
-        public async void GetsProductsAvailable()
+        public async void GetsProductById()
         {
-            var options = TestUtil.GetMemDbOptions("GetsProductsAvailable");
+            var options = TestUtil.GetMemDbOptions("TestProductRepository-GetsProductById");
+
+            Product product;
 
             using (var db = new StoreContext(options))
             {
-                var locationId = Guid.Parse("B8B94F18-D101-4576-AF28-3CBF31EBD6B2");
+                product = TestUtil.NewProduct(db, 10);
+            }
 
-                var productRepo = (IProduct) new ProductRepository(db);
-                var locationRepo = (ILocation) new LocationRepository(db);
-                var location = await locationRepo.GetById(locationId);
-                var products = locationRepo.GetProductsAvailable(location);
-
-                Assert.Equal(products.Count(), 9);
-
-                var quantity = products
-                              .Where(p => p.Item1.ProductId == Guid.Parse("C686DC98-FD97-4073-857F-17DBC48F7CA8"))
-                              .Select(p => p.Item2)
-                              .FirstOrDefault();
-                
-                Assert.Equal(quantity, 220);
+            using (var db = new StoreContext(options))
+            {
+                var repo = (IProduct) new ProductRepository(db);
+                var fromId = await repo.GetProductById(product.ProductId);
+                Assert.Equal(product.ProductId, fromId.ProductId);
             }
         }
     }
